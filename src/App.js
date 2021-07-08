393f15ceac4a7680517384d2368217b5c4d238d1
kim <nahappy15@naver.com>
2021-07-08 11:48:00 +0900

    immutable Array.from, Object.assign 쓰는게 좋음.

--- src/App.js
+++ src/App.js
@@ -45,11 +45,13 @@ class App extends Component {//하나의 최상위 태그가 있어야한다. �
         else if(this.state.mode === 'create'){
             _article = <CreateContent onSubmit={function(_title, _desc){
                 this.max_content_id = this.max_content_id + 1;
-                var _contents = this.state.contents.concat(
-                    {id:this.max_content_id, title:_title, desc:_desc}
-                )
+                var newContents = Array.from(this.state.contents);
+                newContents.push({id:this.max_content_id,
+                title:_title,
+                desc:_desc
+                });
                 this.setState({
-                    contents:_contents
+                    contents:newContents
                 });
             }.bind(this)}></CreateContent>
         }
